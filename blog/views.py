f5795203230031393f369df5b34197581bb54a1f
YaGaskoin <32508888+YaGaskoin@users.noreply.github.com>
2020-03-20 18:25:04 +0300

    add system of comments

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 # Create your views here.
 
@@ -53,4 +53,23 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status="published",
                              publish__year=year, publish__month=month, publish__day=day)
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # Комментарий был опубликован
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создайте объект Comment, но пока не сохраняйте в базу данных
+            new_comment = comment_form.save(commit=False)
+            # Назначить текущий пост комментарию
+            new_comment.post = post
+            # Сохранить комментарий в базе данных
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
