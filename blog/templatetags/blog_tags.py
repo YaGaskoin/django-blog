bb929849716ba64a33111900f6e43d49578f1fa1
YaGaskoin <32508888+YaGaskoin@users.noreply.github.com>
2020-03-21 17:01:57 +0300

    add custom filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -7,3 +9,8 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.objects.filter(status="published").count()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
